da9a714d9fdc74394d98db92995474dc62bb37cb
bansalgantavya <147243873+bansalgantavya@users.noreply.github.com>
2024-04-07 02:56:04 +0530

    quiz done on test

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+
 var questions = [
     "Do you prefer to spend time alone rather than with a large group of people?",
     "Do you focus on the present rather than thinking about the future?",
@@ -17,30 +18,34 @@ var questions = [
     "Do you prefer to be systematic rather than casual?"
 ];
 
-function createQuestionElement(question, index) {
+var currentQuestionIndex = 0;
+var answers = [];
+
+function createQuestionElement(question) {
     var questionElement = document.createElement("div");
     questionElement.className = "question";
     questionElement.innerHTML = `
         <p>${question}</p>
-        <button class="xyz"  onclick="answerQuestion(${index}, 'yes')">Yes</button>
-        
-        <button class="xyz" onclick="answerQuestion(${index}, 'no')">No</button>
+        <button class="yesButton" onclick="answerQuestion('yes')">Yes</button>
+        <button class="noButton" onclick="answerQuestion('no')">No</button>
     `;
     return questionElement;
 }
- 
 
-function renderQuestions() {
+function renderQuestion() {
     var questionsContainer = document.getElementById("questions");
-    questions.forEach(function(question, index) {
-        questionsContainer.appendChild(createQuestionElement(question, index));
-    });
+    questionsContainer.innerHTML = '';
+    questionsContainer.appendChild(createQuestionElement(questions[currentQuestionIndex]));
 }
 
-var answers = [];
-
-function answerQuestion(index, answer) {
-    answers[index] = answer;
+function answerQuestion(answer) {
+    answers[currentQuestionIndex] = answer;
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        renderQuestion();
+    } else {
+        classifyMBTI();
+    }
 }
 
 function classifyMBTI() {
@@ -49,125 +54,26 @@ function classifyMBTI() {
     var thinking = answers[2] === 'yes';
     var judging = answers[3] === 'yes';
 
-    var personalityType = ""; // Placeholder for the personality type
-
-    if (introvert) {
-        if (sensing) {
-            if (thinking) {
-                if (judging) {
-                    personalityType = "ISTJ";
-                } else {
-                    personalityType = "ISTP";
-                }
-            } else {
-                if (judging) {
-                    personalityType = "ISFJ";
-                } else {
-                    personalityType = "ISFP";
-                }
-            }
-        } else {
-            if (thinking) {
-                if (judging) {
-                    personalityType = "INTJ";
-                } else {
-                    personalityType = "INTP";
-                }
-            } else {
-                if (judging) {
-                    personalityType = "INFJ";
-                } else {
-                    personalityType = "INFP";
-                }
-            }
-        }
+    var personalityType = "";
+    
+    // Determine personality type based on answers
+    if (introvert && sensing && thinking && judging) {
+        personalityType = "ISTJ";
+    } else if (introvert && sensing && !thinking && judging) {
+        personalityType = "ISFJ";
+    } else if (!introvert && sensing && !thinking && !judging) {
+        personalityType = "ESTP";
+    } else if (!introvert && sensing && !thinking && judging) {
+        personalityType = "ESTJ";
+    } else if (!introvert && !sensing && thinking && judging) {
+        personalityType = "ENTJ";
     } else {
-        if (sensing) {
-            if (thinking) {
-                if (judging) {
-                    personalityType = "ESTJ";
-                } else {
-                    personalityType = "ESTP";
-                }
-            } else {
-                if (judging) {
-                    personalityType = "ESFJ";
-                } else {
-                    personalityType = "ESFP";
-                }
-            }
-        } else {
-            if (thinking) {
-                if (judging) {
-                    personalityType = "ENTJ";
-                } else {
-                    personalityType = "ENTP";
-                }
-            } else {
-                if (judging) {
-                    personalityType = "ENFJ";
-                } else {
-                    personalityType = "ENFP";
-                }
-            }
-        }
+        personalityType = "Other"; // Default for other cases
     }
 
-    document.getElementById("result").innerHTML = "Your MBTI personality type is: " + personalityType;
+    // Redirect based on personality type
+    var redirectUrl = "assets/" + personalityType.toLowerCase() + ".html";
+    window.location.href = redirectUrl;
 }
 
-renderQuestions();
-
-
-// var redirectUrl = "";
-// switch (personalityType) {
-//   case "ISTJ":
-//     redirectUrl = "assets/istj.html";
-//     break;
-//   case "ISFJ":
-//     redirectUrl = "assets/isfj.html";
-//     break;
-//   case "INFJ":
-//     redirectUrl = "assets/infj.html";
-//     break;
-//   case "INTJ":
-//     redirectUrl = "assets/intj.html";
-//     break;;lk n
-//   case "ISTP":
-//     redirectUrl = "assets/istp.html";
-//     break;
-//   case "ISFP":
-//     redirectUrl = "assets/isfp.html";
-//     break;
-//   case "INFP":
-//     redirectUrl = "assets/infp.html";
-//     break;
-//   case "INTP":
-//     redirectUrl = "assets/intp.html";
-//     break;
-//   case "ESTJ":
-//     redirectUrl = "assets/estj.html";
-//     break;
-//   case "ESFJ":
-//     redirectUrl = "assets/esfj.html";
-//     break;
-//   case "ENFJ":
-//     redirectUrl = "assets/enfj.html";
-//     break;
-//   case "ENTJ":
-//     redirectUrl = "assets/entj.html";
-//     break;
-//   case "ESTP":
-//     redirectUrl = "assets/estp.html";
-//     break;
-//   case "ESFP":
-//     redirectUrl = "assets/esfp.html";
-//     break;
-//   case "ENFP":
-//     redirectUrl = "assets/enfp.html";
-//     break;
-//   case "ENTP":
-//     redirectUrl = "assets/entp.html";
-//     break;
-// }
-// window.location.href = redirectUrl;
+renderQuestion();
